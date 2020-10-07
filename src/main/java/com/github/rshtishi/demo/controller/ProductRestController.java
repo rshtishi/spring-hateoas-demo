d850190ba64583c9d8cb40e8e56e7c371b46ae66
rshtishi <rando.shtishi@gmail.com>
2020-10-07 16:52:55 +0200

    implementing hateous for findall

--- src/main/java/com/github/rshtishi/demo/controller/ProductRestController.java
+++ src/main/java/com/github/rshtishi/demo/controller/ProductRestController.java
@@ -1,8 +1,17 @@
 package com.github.rshtishi.demo.controller;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +22,21 @@ import com.github.rshtishi.demo.repository.ProductRepository;
 @RestController
 @RequestMapping("/products")
 public class ProductRestController {
-	
+
 	@Autowired
 	private ProductRepository productRepository;
-	
+
 	@GetMapping
-	public List<Product> findAllProducts(){
-		return productRepository.findAll();
+	public ResponseEntity<CollectionModel<EntityModel<Product>>> findAll() {
+
+		List<EntityModel<Product>> content = productRepository.findAll().parallelStream()
+				.map(product -> EntityModel.of(product)).collect(Collectors.toList());
+
+		Link link = linkTo(methodOn(ProductRestController.class).findAll()).withSelfRel();
+
+		CollectionModel<EntityModel<Product>> productCollectionModel = CollectionModel.of(content, link);
+
+		return ResponseEntity.ok(productCollectionModel);
 	}
 
 }
